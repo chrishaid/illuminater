15d848034c502b5edda7ba89ba4d794a775e8fa9
Chris Haid <chaid@kippchicago.org>
2015-10-08 10:19:53 -0500

    Added test for mastery_grid_plot.

--- tests/testthat/test_mastery_plots.R
+++ tests/testthat/test_mastery_plots.R
@@ -3,7 +3,7 @@ require(dplyr)
 data("ex_ill_obj")
 
 
-p <- mastery_long_plot(ex_ill_obj$results,
+p_long <- mastery_long_plot(ex_ill_obj$results,
                   ex_ill_obj$roster %>%
                     filter(school_initials == "BCS",
                            grade_level == 8),
@@ -12,9 +12,31 @@ p <- mastery_long_plot(ex_ill_obj$results,
                   assm_subj == "math",
                   assm_unit == "u1")
 
-p_build <- ggplot_build(p)
+p_long_build <- ggplot_build(p_long)
+
+
+p_grid <- mastery_grid_plot(ex_ill_obj$results,
+                            ex_ill_obj$roster %>%
+                              filter(school_initials == "BCS",
+                                     grade_level == 8,
+                                     home_room == "University of Oregon"),
+                            school_id = 4,
+                            school_name = "BCS",
+                            assm_subj == "math",
+                            assm_unit == "u1")
+
+p_grid_build <- ggplot_build(p_grid)
+
+
+
 
 test_that("mastery_long_plot generates a plot", {
-  expect_is(p, c("gg", "ggplot"))
-  expect_equal(sum(p_build$data[[1]]$ymax), 71.5)
+  expect_is(p_long, c("gg", "ggplot"))
+  expect_equal(sum(p_long_build$data[[1]]$ymax), 71.5)
+})
+
+
+test_that("mastery_grid_plot generates a plot", {
+  expect_is(p_grid, c("gg", "ggplot"))
+  expect_equal(sum(p_grid_build$data[[1]]$ymax), 3877.5)
 })
